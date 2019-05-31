d05d5a01a38e29af60e8255cf2ecf351e530bd54
Pradyuman <meetupradyuman@gmail.com>
2019-05-31 08:13:49 +0530

    P5.js basic structure

--- SRIP/Codes/main.js
+++ SRIP/Codes/main.js
@@ -1,24 +1,15 @@
-var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+function setup() {
+	createCanvas(innerWidth, innerHeight);
+	background(135,206,235);
 
-			var renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+	//soil
+	fill(109,88,74);
+	
+	stroke(127, 63, 120);
+	rect(300,300,300,300);
 
-			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			var cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+}
 
-			camera.position.z = 5;
+function draw() {
 
-			var animate = function () {
-				requestAnimationFrame( animate );
-
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
-
-				renderer.render( scene, camera );
-			};
-
-			animate();
+}
