d0e98bf02d009f741391a7a49b216b03f527acfe
legendary-acp <meetupradyuman@gmail.com>
2019-06-29 13:28:38 +0530

    simple button -> reset button

--- SRIP/Codes/elements.js
+++ SRIP/Codes/elements.js
@@ -10,13 +10,13 @@ class soil {
 
     compress(n) {
         if (this.height > n) {
-            this.y = this.y + 0.5;
-            this.height = this.height - 0.5;
+            this.y = this.y + 2;
+            this.height = this.height - 2;
             console.log(this.height);
         }
-        
+
     }
-    expand(n){
+    expand(n) {
         if (this.height < n) {
             this.y = this.y - 1;
             this.height = this.height + 1;
@@ -31,44 +31,52 @@ class soil {
 class weight {
 
     constructor(x) {
-        this.x1 = x;
+        this.x1 = x + 7;
         this.y1 = 250;
-        this.x2 = this.x1 + 100;
+        this.x2 = this.x1 + 87;
         this.y2 = 250;
         this.x3 = (this.x1 + this.x2) / 2;
         this.y3 = 190;
     }
 
-    move_down(wtn,n) {
+    move_down(wtn, n) {
         if (this.y2 < n && wtn % 2 == 1) {
-            this.y1 = this.y1 + 0.5;
-            this.y2 = this.y2 + 0.5;
-            this.y3 = this.y3 + 0.5;
-        }
-        else if (this.y2 < n && wtn % 2 == 0) {
-            this.y1 = this.y1 + 0.5;
-            this.y2 = this.y2 + 0.5;
-            this.y3 = this.y3 + 0.5;
-        }
-        else{
+            this.y1 = this.y1 + 2;
+            this.y2 = this.y2 + 2;
+            this.y3 = this.y3 + 2;
+        } else if (this.y2 < n && wtn % 2 == 0) {
+            this.y1 = this.y1 + 2;
+            this.y2 = this.y2 + 2;
+            this.y3 = this.y3 + 2;
+        } else {
             wtn = wtn + 1;
         }
         return wtn;
     }
 
-    show() {
+    show(i) {
         fill(0)
         stroke(0);
         strokeWeight(20);
         strokeJoin(ROUND);
         triangle(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3);
         noStroke();
+        fill(255);
+        textSize(15);
+        switch (i) {
+            case 0:
+                text('1 Tonne', this.x1 + 20, this.y2 - 10);
+                break;
+            case 1:
+                text('5 Tonne', this.x1 + 20, this.y2 - 10);
+                break;
+            case 2:
+                text('10 Tonne', this.x1 + 15, this.y2 - 10);
+                break;
+            case 3:
+                text('10 Tonne', this.x1 + 20, this.y2 - 10);
+                break;
+        }
+        fill(0);
     }
 };
-
-/*
-rect(100, 425, 200, 125);
-rect(400, 375, 200, 175);
-rect(700, 375, 200, 175);
-rect(1000, 375, 200, 175);
-*/
--- SRIP/Codes/main.js
+++ SRIP/Codes/main.js
@@ -8,10 +8,70 @@ function setup() {
 	var x = (windowWidth - width) / 2;
 	var y = (windowHeight - height) / 2;
 	cnv.position(x, y);
-	background(255, 0, 200);
 
+	background(255);
 	noStroke();
 
+}
+
+function draw() {
+	background(255);
+
+	fill(0);
+	textSize(32);
+	text('Consolidation Test', (windowWidth - width) / 2 + 200, 50);
+
+	if (start % 2 == 1) {
+		s[0].show();
+		w[0].show(0);
+		s[1].show();
+		w[1].show(1);
+		s[2].show();
+		w[2].show(2);
+		s[3].show();
+
+		switch (wtn) {
+			case 1:
+				wtn = w[0].move_down(wtn, 415);
+				break;
+			case 2:
+				s[1].compress(50);
+				wtn = w[0].move_down(wtn, 420);
+				break;
+			case 3:
+				wtn = w[1].move_down(wtn, 415);
+				break;
+			case 4:
+				s[2].compress(45);
+				wtn = w[1].move_down(wtn, 430);
+				break;
+			case 5:
+				wtn = w[2].move_down(wtn, 415);
+				break;
+			case 6:
+				s[3].compress(40);
+				wtn = w[2].move_down(wtn, 435);
+				break;
+		}
+
+		if (wtn == 7) {
+			s[4].height = 40;
+			s[4].y = 445;
+			w[3].y1 = w[3].y2 = 445;
+			w[3].y3 = 385;
+
+			
+		}
+	}
+}
+
+function strt() {
+	start = 1;
+	stuff();
+	wtn = 1;
+}
+
+function stuff() {
 	s[0] = new soil(100);
 	s[1] = new soil(250);
 	s[2] = new soil(400);
@@ -25,59 +85,4 @@ function setup() {
 	w[3] = new weight(700);
 	w[4] = new weight(750);
 
-
-}
-
-function draw() {
-	background(135, 206, 235);
-
-	fill(0);
-	textSize(32);
-	text('Consolidation Test', (windowWidth - width) / 2 + 200, 50);
-
-	if (start == 1) {
-		s[0].show();
-		w[0].show();
-		s[1].show();
-		w[1].show();
-		s[2].show();
-		w[2].show();
-		s[3].show();
-		
-		switch (wtn) {
-			case 1:
-				wtn = w[0].move_down(wtn,415);
-				break;
-			case 2:
-				s[1].compress(50);
-				wtn = w[0].move_down(wtn,420);
-				break;
-			case 3:
-				wtn = w[1].move_down(wtn,415);
-				break;
-			case 4:
-				s[2].compress(45);
-				wtn = w[1].move_down(wtn,430);
-				break;
-			case 5:
-				wtn = w[2].move_down(wtn,415);
-				break;
-			case 6:
-				s[3].compress(40);
-				wtn = w[2].move_down(wtn,435);
-				break;
-		}
-		
-		if(wtn==7){
-			s[4].height = 40;
-			s[4].y = 445;
-			w[3].y1 = w[3].y2 = 445;
-			w[3].y3 = 385;
-			
-		}
-	}
-}
-
-function temp() {
-	start = 1;
 }
