66fc52ef097feac3da8157e4fd98abb3e0b89381
legendary-acp <meetupradyuman@gmail.com>
2019-07-29 00:17:21 +0530

    Top of tool design initiated

--- SRIP 2/Codes/elements.js
+++ SRIP 2/Codes/elements.js
@@ -1,17 +1,23 @@
+var ch = wh * 0.95,
+    strth = 0;
+
+var cw = ww * 0.8,
+    strtw = 0;
+
 class soil {
 
     constructor(x1) {
-        this.x = Math.floor(x1 / 1000 * window.innerWidth * 0.73);
-        this.width = Math.floor(100 / 1000 * window.innerWidth * 0.73);
-        this.height = Math.floor(60 / 610 * window.innerHeight * 0.95);
-        this.y = Math.floor(425 / 610 * window.innerHeight * 0.95);
+        this.x = Math.floor(x1 / 1000 * ww * 0.73);
+        this.width = Math.floor(100 / 1000 * ww * 0.73);
+        this.height = Math.floor(60 / 610 * wh * 0.95);
+        this.y = Math.floor(425 / 610 * wh * 0.95);
         this.ln = [];
         this.strk();
 
     }
 
     strk() {
-        this.lh = Math.floor(425 / 610 * window.innerHeight * 0.95) + 1;
+        this.lh = Math.floor(425 / 610 * wh * 0.95) + 1;
         while (this.lh < (this.y + this.height)) {
             this.wid = 0;
             while (this.wid < (this.width * 0.88)) {
@@ -23,18 +29,9 @@ class soil {
         }
     }
 
-    compress(n) {
-        if (this.height > n) {
-            this.y = this.y + 1;
-            this.height = this.height - 1;
-        }
-    }
-
-    expand(n) {
-        if (this.height < (n / 100 * this.height)) {
-            this.y = this.y - 0.25;
-            this.height = this.height + 0.25;
-        }
+    move_left(n) {
+        if (this.x > n)
+            this.x = this.x - 1;
     }
 
     show() {
@@ -50,5 +47,80 @@ class soil {
             this.it += 1;
         }
         noStroke();
+    }
+};
+
+class upper_tool {
+    constructor() {
+        this.x2 = strtw + cw * 0.2;
+        this.y2 = strth + 0.4 * ch;
+        this.height2 = 0.05 * ch;
+        this.width2 = ch * 0.2;
+
+        this.x1 = this.x2;
+        this.y1 = this.y2 + this.height2;
+        this.height1 = ch * 0.15;
+        this.width1 = this.height2;
+
+        this.x3 = this.x2 + this.width2 - this.height2;
+        this.y3 = this.y1;
+        this.height3 = ch * 0.2;
+        this.width3 = this.height2;
+
+        
+        this.x5 = this.x2 + this.width2;
+        this.y5 = this.y2 + strth + 0.13 * ch;
+        this.height5 = this.height2;
+        this.width5 = strtw + 0.3 * cw;
+
+        this.x4 = this.x5 + this.width5;
+        this.y4 = this.y1;
+        this.height4 = this.height2 + this.height3;
+        this.width4 = this.height2;
+
+        this.x6 = this.x5 + 0.1 * cw;
+        this.y6 = 0.55 * ch;
+        this.height6 = this.height2;
+        this.width6 = 0.4 * cw;
+        this.wdt = 0.05 * cw;
+
+        this.x7 = this.x6 + 0.15 * cw;
+        this.y7 = this.y2 + strth + 0.5 * ch;
+        this.height7 = 0.05 * ch;
+        this.width7 = 0.1 * cw;
+
+        this.x8 = strtw + cw * 0.1;
+        this.y8 = strth + ch * 0.2;
+        this.height8 = ch * 0.75;
+        this.width8 = ch * 0.025;
+
+        this.x9 = this.x8;
+        this.y9 = this.y8 + this.height8;
+        this.height9 = this.width8;
+        this.width9 = cw * 0.9;
+
+        this.x10 = this.x5 +(this.width5/2) + 0.2 * cw;
+        this.y10 = this.y2 + strth + 0.45 * ch;
+        this.rad = 0.05 * ch;
+    }
+
+    show() {
+        fill(0);
+        rect(this.x1, this.y1, this.width1, this.height1);
+        rect(this.x2, this.y2, this.width2, this.height2);
+        rect(this.x3, this.y3, this.width3, this.height3);
+        rect(this.x4, this.y4, this.width4, this.height4);
+        rect(this.x5, this.y5, this.width5, this.height5);
+        fill(0);
+        rect(this.x7, this.y7, this.width7, this.height7);
+        fill(0);
+        rect(this.x8, this.y8, this.width8, this.height8);
+        rect(this.x9, this.y9, this.width9, this.height9);
+        rect(this.x6, this.y6, this.width6, this.height6);
+        triangle(this.x6, this.y6, this.x6, this.y6 + this.height,
+            this.x6 - this.wdt, this.y6 + this.height);
+        triangle(this.x6 + this.width6, this.y6, this.x6 + this.width6,
+            this.y6 + this.height, this.x6 + this.wdt + this.width6, this.y6 + this.height);
+        circle(this.x10, this.y10, this.rad);
     }
 };
--- SRIP 2/Codes/main.js
+++ SRIP 2/Codes/main.js
@@ -1,22 +1,28 @@
-let s = [];
+let ut;
 var start = 0;
 let wtn = 1;
 var change = 0;
-
+var ww = window.innerWidth,
+	wh = window.innerHeight;
+function init() {
+		start = 1;
+		stuff();
+		wtn = 1;
+	}
 function setup() {
 	noStroke();
 	stuff();
 }
 
 function draw() {
-	var cnv = createCanvas(window.innerWidth * 0.73, window.innerHeight * 0.95);
+	var cnv = createCanvas(ww * 0.73, wh * 0.95);
 	var x = (windowWidth - width) / 2;
 	var y = (windowHeight - height) / 2;
 	cnv.position(x, y);
 
 	//not working
-	if (change == window.innerHeight + window.innerWidth) {
-		change = window.innerHeight + window.innerWidth;
+	if (change == wh + ww) {
+		change = wh + ww;
 		stuff();
 	}
 	background(255);
@@ -24,20 +30,16 @@ function draw() {
 
 
 	fill(0);
-	textSize(4 / 3 * window.innerWidth * 0.02);
-	text('Consolidation Test', (windowWidth - width) / 2 + 5 * window.innerWidth * 0.02, window.innerWidth * 0.035);
+	textSize(4 / 3 * ww * 0.02);
+	text('Direct Shear Test', (windowWidth - width) / 2 + 5 * ww * 0.02, ww * 0.035);
 
 	if (start % 2 == 1) {
-
+		ut.show();
 	}
 }
 
-function strt() {
-	start = 1;
-	stuff();
-	wtn = 1;
-}
+
 
 function stuff() {
-
+	ut = new upper_tool();
 }
